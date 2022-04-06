eee9a185705108f35961a084de68ab3621e20ef1
StanciuVictor <victor.stanciu@yahoo.com>
2022-04-06 15:36:57 +0300

    Add GET /tasks -> Get all tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,19 @@
 // The Controller is the entry point, comunicates with the Service and returns the result
+// The Controller's only job is to recieve a request, delegate it to where it's needed to achieve the goal, and then return the response
 
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 // For the route /tasks, let this Controller handle it
 @Controller('tasks')
 export class TasksController {
-  // private, public, etc
   // Make tasksService a private property of TasksController class, and with the type of TasksService
   constructor(private tasksService: TasksService) {}
+
+  // Whenever a GET request on '/tasks' comes in, this hadler method takes care of it
+  @Get()
+  getAllTasks() {
+    // Returns the tasks array from TasksService
+    return this.tasksService.getAllTasks();
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,4 +5,14 @@ import { Injectable } from '@nestjs/common';
 // Making this @Injectable, makes it a SINGLETON
 // that can be shared across the application
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  // Store the tasks until we implement databases. Making them PRIVATE is a good practice
+  private tasks = [];
+
+  /**
+   * Creates a way to access the tasks array (which is private) from the outside of this class.
+   */
+  getAllTasks() {
+    return this.tasks;
+  }
+}
