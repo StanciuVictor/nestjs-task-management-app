25703fb72a87296aa38868f2b5ac1ebc09bc759d
StanciuVictor <victor.stanciu@yahoo.com>
2022-04-06 14:13:58 +0300

    Inject tasksService in tasksController - not finished

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,10 @@
 import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 // For the route /tasks, let this Controller handle it
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  // private, public, etc
+  // Make tasksService a private property of TasksController class, and with the type of TasksService
+  constructor(private tasksService: TasksService) {}
+}
