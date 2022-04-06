0d5c50a3ac7370c2bed8039254122ee75026505c
StanciuVictor <victor.stanciu@yahoo.com>
2022-04-06 13:12:23 +0300

    Add new Service called 'tasks' with 'nest g service tasks --no-spec'

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 
 @Module({
   controllers: [TasksController],
+  // This allows us to inject the Service intro the Controller,
+  // since the Controller is defined in this Module and the
+  // Service is defined as a provider in this Module.
+  // !! The Service is injectible only if it has the @Injectable decorator !!
+  providers: [TasksService],
 })
 export class TasksModule {}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+
+// Making this @Injectable, makes it a SINGLETON
+// that can be shared across the application
+@Injectable()
+export class TasksService {}
