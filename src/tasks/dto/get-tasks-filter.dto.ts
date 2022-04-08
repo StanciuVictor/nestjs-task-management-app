7a6e459d87ac640e40fa336d7cdeae2456071a7a
StanciuVictor <victor.stanciu@yahoo.com>
2022-04-08 19:03:49 +0300

    Add validation for filtering tasks: Throws error if search contains chars other than a-z A-Z

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,9 +1,15 @@
 // This DTO holds the values of the query parameters ( filters for the search )
 
+import { IsAlpha, IsEnum, IsOptional } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
   // The ? marks the parameter as OPTIONAL, since we might not want to provide any filter at all
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsAlpha()
   search?: string;
 }
