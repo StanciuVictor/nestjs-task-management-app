ba06c68750b09b780a3a5d431be82c98243e5dbe
StanciuVictor <victor.stanciu@yahoo.com>
2022-04-08 16:01:30 +0300

    Add error handling for deleting a task that doesn't exist

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -110,7 +110,8 @@ export class TasksService {
    * @param {string} id
    */
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id !== found.id);
   }
 
   /**
