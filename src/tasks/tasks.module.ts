0def32ad7042689146f35d7273cccfcd557ba880
StanciuVictor <victor.stanciu@yahoo.com>
2022-04-11 11:52:45 +0300

    Downgrade to typeorm@0.2.45 and Create a Tasks Repository

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksController } from './tasks.controller';
+import { TasksRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
 
 @Module({
+  // Inject TasksRepository as a dependency wherever we need it in the module
+  imports: [TypeOrmModule.forFeature([TasksRepository])],
   controllers: [TasksController],
   // This allows us to inject the Service intro the Controller,
   // since the Controller is defined in this Module and the
