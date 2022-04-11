c955c22fc54fef8fd4685b08a13b12bb680a3ab2
StanciuVictor <victor.stanciu@yahoo.com>
2022-04-11 10:08:02 +0300

    Setting up database conection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,24 @@
 
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      // Configuration values
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'task-management',
+      // Automatically load entities (defined in nestjs) that translate to tables and schemas (using typeorm)
+      autoLoadEntities: true,
+      // Always keep the DB schema in sync.
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
