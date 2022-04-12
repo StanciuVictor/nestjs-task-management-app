43f5ae8b892db888d46ed96ec977c114501383fe
StanciuVictor <victor.stanciu@yahoo.com>
2022-04-12 12:37:50 +0300

    Add sign up feature

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  /**
+   * When a POST req comes on /signup route,
+   * the app sends the parameters from the body of the req to the Service
+   *
+   * @param {AuthCredentialsDto} authCredentialDto
+   * @return {*}  {Promise<void>}
+   * @memberof AuthController
+   */
+  @Post('/signup')
+  signUp(@Body() authCredentialDto: AuthCredentialsDto): Promise<void> {
+    return this.authService.signUp(authCredentialDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
@@ -8,4 +9,15 @@ export class AuthService {
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
   ) {}
+
+  /**
+   * Takes the user credentials and sends them to the Repository to create a new user
+   *
+   * @param {AuthCredentialsDto} authCredentialDto
+   * @return {*}  {Promise<void>}
+   * @memberof AuthService
+   */
+  async signUp(authCredentialDto: AuthCredentialsDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialDto);
+  }
 }
