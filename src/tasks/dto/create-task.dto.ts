da487bda68968833892776e1e1459dfd419598ae
StanciuVictor <victor.stanciu@yahoo.com>
2022-04-12 12:38:29 +0300

    Remove typo

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,6 +1,6 @@
 /**
  * Define the DTO as a TS class (using the naming convention for DTO classes in NestJS)
- * This DTO helps, because if I want to modify the params, I cand change them here,
+ * This DTO helps, because if I want to modify the params, I can change them here,
  * and not go through all the files and modify them.
  */
 
