aa4176ad55e6760bbde9b3b17c7c807e79fab260
StanciuVictor <victor.stanciu@yahoo.com>
2022-04-12 13:36:02 +0300

    Add validation: username and password strength

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -4,7 +4,20 @@
  * and not go through all the files and modify them.
  */
 
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must cointain at least: 1 upper case letter, 1 lowercase letter, 1 number or special character',
+  })
   password: string;
 }
