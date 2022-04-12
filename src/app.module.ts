a59780f3ab44271693868dff23954df0794c11f2
StanciuVictor <victor.stanciu@yahoo.com>
2022-04-12 11:22:43 +0300

    Set up AuthModule, AuthService, AuthController, UserEntity, UsersRepository

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TasksModule,
+    AuthModule,
     TypeOrmModule.forRoot({
       // Configuration values
       type: 'postgres',
