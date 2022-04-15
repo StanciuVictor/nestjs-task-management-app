6e662edaac56220538eb904389e8440e09bb0362
StanciuVictor <victor.stanciu@yahoo.com>
2022-04-15 15:35:55 +0300

    Set up ConfigModule for configuration management

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    // ConfigModule loads the configuration for a specific stage
+    ConfigModule.forRoot({
+      /* STAGE tells the app the environment it is in (dev or prod) so that the
+      ConfigModule loads the correct environment file (.env.stage.dev or prod)
+      STAGE is defined in package.json -> scripts */
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
     TasksModule,
     AuthModule,
     TypeOrmModule.forRoot({
