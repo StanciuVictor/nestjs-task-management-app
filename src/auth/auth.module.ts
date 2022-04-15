25e1724a08811700ff3eaaa83a672d52b72893ef
StanciuVictor <victor.stanciu@yahoo.com>
2022-04-15 17:50:25 +0300

    Add JWT Secret Configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,17 +6,25 @@ import { UsersRepository } from './users.repository';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
     // This module exports a service called JWT Service
-    JwtModule.register({
-      secret: 'topSecret51',
-      signOptions: {
-        // JWT token expires in 3600 seconds = 1 hour
-        expiresIn: 3600,
-      },
+    // Is async because we wait for the ConfigModule initialization to finish (see above), to then be available for Dependency Injection
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      // Whatever this func returns is going to be the config for this module
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          // JWT token expires in 3600 seconds = 1 hour
+          expiresIn: 3600,
+        },
+      }),
     }),
     TypeOrmModule.forFeature([UsersRepository]),
   ],
